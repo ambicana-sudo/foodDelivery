f4e4cad643f1478a8fcdfc1d6a301343377b6963
Ambica <ambicana5@gmail.com>
2022-11-24 18:43:24 +0545

    Added Restaurant API

--- server/src/app.js
+++ server/src/app.js
@@ -11,8 +11,10 @@ app.use(cors());
 app.use(bodyParser.json());
 const registerRouter = require("./Controller/registerRouter");
 const loginRouter = require("./Controller/loginRouter");
+const restroRouter = require("./Controller/restaurantRouter");
 app.use("/register", registerRouter);
 app.use("/login", loginRouter);
+app.use("/restaurant", restroRouter);
 
 app.listen(process.env.PORT, () => {
   console.log(`Chat Server listening on port ${process.env.PORT}`);
